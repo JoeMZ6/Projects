be5b1981c04f83322cf419220449406dfb28074f
Yousef_Zaki <yousefzaki293@gmail.com>
2025-04-05 03:58:59 +0200

    Dice game.py

--- Dice game.py
+++ Dice game.py
@@ -1,11 +1,11 @@
 import random
 while True:
-    print("roll the dice (y,n):")
-    input_char=str(input())
-    if input_char =='n'or input_char=='N':
+    print("roll the dice (y,n)??:")
+    input_char=str(input().split().lower())
+    if input_char =='n':
         print("thanks for playing")
         break
-    elif input_char=="y" or input_char=='Y':
+    elif input_char=="y" :
         die1=random.randint(1,6)
         
         die2=random.randint(1,6)
