5a3884e60554ef9c48c8ad6b1f58fcab937d998d
Yousef_Zaki <yousefzaki293@gmail.com>
2025-06-04 16:11:19 +0300

    Update Gambling Game(multiplayer version).py
    Signed-off-by: Yousef_Zaki <yousefzaki293@gmail.com>

--- Gambling Game(multiplayer version).py
+++ Gambling Game(multiplayer version).py
@@ -13,37 +13,56 @@ player_score = [0] * number_of_players
 max_score = int(input("Enter the winning score (e.g., 50):\n"))
 
 while max(player_score) < max_score:
+
     for player_idx in range(number_of_players):
+        print('#' * 150)
         print(f"\nPlayer {player_idx + 1}'s turn has just started! 🚀")
         print(f"Your total score is {player_score[player_idx]}")
 
-       
         current_score = 0
         while True:
-            
+            if player_score[player_idx] >= max_score:
+                print(f"\n🎉 Player {player_idx + 1} just won with a score of {player_score[player_idx]}! 🎉\n")
+                for i in range(number_of_players):
+                    print(f"Player {i + 1} score: {player_score[i]}")
+
+                exit()  
+               
             while True:
                 should_roll = input("Do you want to roll? (y/n): 🤔 ").lower()
                 if should_roll in ['y', 'n']:
                     break
-                print("Invalid input. Please enter 'y' to roll or 'n' to stop.")
+                print("Invalid input. Please enter y to roll or n to stop.")
 
             if should_roll == 'y':
+                
                 value = roll()
                 print(f"You rolled a {value}!")
 
                 if value == 1:
+                    player_score[player_idx]-=current_score
                     current_score = 0
-                    player_score[player_idx] += current_score  
                     print(f"You rolled a 1! Your turn ends, and your score in this round is {current_score} 😓")
                     break
                 else:
                     current_score += value
-                    print(f"Your current turn score is {current_score}, and your total score would be {player_score[player_idx] + current_score}. 🎲")
+                    player_score[player_idx]+=value
+                    print(f"Your current turn score is {current_score}, and your total score would be {player_score[player_idx]}. 🎲")
             else:
-                player_score[player_idx] += current_score
                 print(f"You chose to stop rolling. Your total score is now {player_score[player_idx]}.")
                 break
 
-        if player_score[player_idx] >= max_score:
-            print(f"\n🎉 Player {player_idx + 1} just won with a score of {player_score[player_idx]}! 🎉")
-            exit()  
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
